bcea885ff3f6fbea907b77699f33ef1b821c1e2e
Peng Xue Vang <pxvang33@gmail.com>
2019-02-04 05:14:23 -0600

    added table for admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -5,9 +5,26 @@ import AdminDelete from './AdminDelete';
 
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Projects from '../Projects/Projects.js'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
+{/* <Paper className={classes.root}>
+    <Table >
+        <TableHead>
+            <TableRow>
+                <TableCell>Project Name</TableCell>
+                <TableCell align="right">Delete</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
 
-
+        </TableBody>
+    </Table>
+</Paper> */}
 
 class Admin extends Component {
     componentDidMount() {
@@ -34,7 +51,26 @@ class Admin extends Component {
                     <h2>Admin</h2>
                     <AdminForm />
                 </div>
+                <br />
+                <br />
                 <div>
+                    <Paper >
+                        <Table className="table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Project Name</TableCell>
+                                    <TableCell align="right">Delete</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.props.reduxStore.projects.map((project) => {
+                                    return <AdminDelete key={project.id} project={project} />
+                                })}
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                </div>
+                {/* <div>
                     <table className="projectTable">
                         <thead>
                             <tr>
@@ -48,7 +84,7 @@ class Admin extends Component {
                             })}
                         </tbody>
                     </table>
-                </div>
+                </div> */}
             </div>
         )
     }
--- src/components/Admin/AdminDelete.js
+++ src/components/Admin/AdminDelete.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 class AdminDelete extends Component {
 
@@ -9,10 +11,14 @@ class AdminDelete extends Component {
     render() {
 
         return (
-            <tr >
-                <td>{this.props.project.name}</td>
-                <td><button>Delete</button></td>
-            </tr>
+            <TableRow>
+                <TableCell>{this.props.project.name}</TableCell>
+                <TableCell align="right"><button>Delete</button></TableCell>
+            </TableRow>
+            // <tr >
+            //     <td>{this.props.project.name}</td>
+            //     <td><button>Delete</button></td>
+            // </tr>
         );
     }
 }
